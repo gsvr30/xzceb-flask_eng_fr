b63c742b12c17c78d91fe8354067ecbfcd9fb9ae
gsvr30 <36774910+gsvr30@users.noreply.github.com>
2021-08-24 18:51:08 +0530

    Update tests.py

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -9,7 +9,7 @@ class TestEnglishToFrench(unittest.TestCase):
         self.assertNotEqual(english_to_french('text'), '')
 
 
-class testFrenchToEnglish(unittest.TestCase):
+class TestFrenchToEnglish(unittest.TestCase):
     def test1(self):
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
         self.assertNotEqual(french_to_english('text'), '')
