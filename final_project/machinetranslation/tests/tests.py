f82ae80bc2b9800822619119453cae7a61638af4
gsvr30 <gsvr30@gmail.com>
2021-08-24 12:59:52 +0000

    changes in test file

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -6,12 +6,12 @@ from translator import english_to_french, french_to_english
 class TestEnglishToFrench(unittest.TestCase):
     def test1(self):
         self.assertEqual(english_to_french('Hello'), 'Bonjour')
-        self.assertIsNotNone(english_to_french(''), 'Test value is none.')
+        self.assertNotEqual(english_to_french(''), '')
 
 
 class testFrenchToEnglish(unittest.TestCase):
     def test1(self):
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
-        self.assertIsNotNone(french_to_english(''), 'Test value is none.')
+        self.assertNotEqual(french_to_english(''), '')
 
 unittest.main()
